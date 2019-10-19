12e03a24ed0163a6ad55f1fa1adeebdb3e7e69c6
BKramek <55166342+BKramek@users.noreply.github.com>
2019-10-19 12:46:24 -0500

    Add files via upload

---
+++ PandaPoints/src/Node.java
@@ -0,0 +1,19 @@
+public class Node {
+	
+	private int index;
+	private Node next;
+	
+	public Node(int idx, Node n) {
+		index = idx;
+		next = n;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public Node getNext() {
+		return next;
+	}
+	
+}
